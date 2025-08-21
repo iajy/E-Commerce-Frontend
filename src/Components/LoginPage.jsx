8cd69aea8b15d39d71e4790b67ac08401f39aa43
Ajay Binu <ajaybinuajay2002@gmail.com>
2025-08-21 14:36:30 +0530

    moving in between the login and signup page

--- src/Components/LoginPage.jsx
+++ src/Components/LoginPage.jsx
@@ -1,14 +1,9 @@
 import React, { useState } from "react";
 import { IoCloseOutline } from "react-icons/io5";
-import LoginPage from "./LoginPage";
 
-const SignUpPage = ({ onExit,onLogin }) => {
+const LoginPage = ({ onExit, onSignUp }) => {
   const [password, setPassword] = useState("");
-  const [vPassword, setVPassword] = useState("");
 
-  const handleVPassword = (e) => {
-    setVPassword(e.target.value);
-  };
   const handlePassword = (e) => {
     setPassword(e.target.value);
   };
@@ -17,21 +12,13 @@ const SignUpPage = ({ onExit,onLogin }) => {
     <div className="fixed inset-0 flex items-center justify-center bg-black/60 z-50">
       <div className="w-90 absolute flex flex-col gap-4 p-4  bg-white rounded-2xl shadow-lg">
         <div className="flex justify-between ">
-          <h1 className="text-2xl font-bold ">SignUp</h1>
+          <h1 className="text-2xl font-bold ">Login</h1>
           <IoCloseOutline
             onClick={onExit}
             className="cursor-pointer text-2xl hover:text-red-600"
           />
         </div>
         <form action="submit" className="flex flex-col gap-4">
-          <input
-            type="text"
-            name="user"
-            required
-            // id=""
-            placeholder="Enter your Name"
-            className="border border-[#C1DCDC]  rounded-full focus:outline-blue-600 focus:outline-1 p-2"
-          />
           <input
             type="text"
             name="email"
@@ -49,28 +36,22 @@ const SignUpPage = ({ onExit,onLogin }) => {
             placeholder="Enter Password"
             className="border border-[#C1DCDC] rounded-full focus:outline-blue-600 focus:outline-1 p-2"
           />
-          <input
-            type="text"
-            name="vPassword"
-            value={vPassword}
-            required
-            // id=""
-            onChange={handleVPassword}
-            placeholder="Verify Password"
-            className={`border ${
-              password === vPassword ? "border-green-600" : "border-red-600"
-            } rounded-full p-2 focus:outline-none`}
-          />
+
           <button className="bg-blue-600 px-6 py-2 rounded-full text-white font-medium cursor-pointer">
-            SignUp
+            Login
           </button>
-          
         </form>
-        <div className="flex justify-around"><p>Already have an account ? <a  onClick={onLogin} className="text-blue-600 cursor-pointer">Login</a></p></div>
+        <div className="flex justify-around">
+          <p>
+            Don't have an account ?{" "}
+            <a onClick={onSignUp} className="text-blue-600 cursor-pointer">
+              SignUp
+            </a>
+          </p>
+        </div>
       </div>
-      
     </div>
   );
 };
 
-export default SignUpPage;
+export default LoginPage;
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -3,13 +3,23 @@ import { CgProfile } from "react-icons/cg";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 import { IoCartOutline } from "react-icons/io5";
 import SignUpPage from "./SignUpPage";
+import LoginPage from "./LoginPage";
 
 const NavBar = () => {
-  const [signUp, setSignUp] = useState(false);
+  // const [signUp, setSignUp] = useState(false);
+  // const [login, setLogin] = useState(false);
 
-  const handleSignUp = () => {
-    setSignUp((prev)=>!prev);
-  };
+  // const handleLogin = () => {
+  //   setLogin((prev)=>!prev);
+  // };
+
+  // const handleSignUp = () => {
+  //   setSignUp((prev)=>!prev);
+  // };
+
+  const [view, setView] = useState("");
+
+
   return (
     <header className="fixed  w-full z-50 bg-white flex justify-between items-center py-5 md:px-10 px-5 shadow-lg ">
       <div className="flex items-center gap-15">
@@ -25,14 +35,15 @@ const NavBar = () => {
           <IoCartOutline className="hover:text-[#C1dcdc]" />
         </p>
         <p>
-          <CgProfile onClick={handleSignUp} className="hover:text-[#C1dcdc]" />
+          <CgProfile onClick={()=>setView("login")} className="hover:text-[#C1dcdc]" />
         </p>
         <p>|</p>
         <p>
           <HiOutlineMenuAlt3 className="hover:text-[#C1dcdc]" />
         </p>
       </div>
-      {signUp && <SignUpPage onExit={handleSignUp} />}
+      {view==="login" && <LoginPage onExit={()=>setView("")} onSignUp={()=>setView("signUp")}/>}
+      {view==="signUp" && <SignUpPage onExit={()=>setView("")} onLogin={()=>setView("login")}/>}
     </header>
   );
 };
