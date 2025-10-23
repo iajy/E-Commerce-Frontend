99878d75c3b35e065068f486703b2dd2f2444886
Ajay Binu <ajaybinuajay2002@gmail.com>
2025-10-23 22:32:02 +0530

    pause the ongoing  offer div by useAnimatuion

--- src/Components/OnGoingOffer.jsx
+++ src/Components/OnGoingOffer.jsx
@@ -1,27 +1,39 @@
 import React, { useEffect, useState } from "react";
 import { FaAngleRight } from "react-icons/fa";
 // eslint-disable-next-line no-unused-vars
-import { motion } from "framer-motion";
+import { motion, useAnimation } from "framer-motion";
 import axios from "axios";
-
+import ProductView from "./ProductView";
 
 const OnGoingOffer = () => {
-  const [offerPlants, setOfferPlants] = useState([]);
+  const [products, setProducts] = useState([]); // offer products
+  const [view, setView] = useState(false);
+  const [plantId, setPlantId] = useState(null);
+  const controls = useAnimation(); // animation controller
 
   useEffect(() => {
     const fetchOffer = async () => {
       try {
         const res = await axios.get("http://localhost:8080/plantbytopoffer");
-        setOfferPlants(res.data);
+        setProducts(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchOffer();
   }, []);
+  useEffect(() => {
+    controls.start({
+      x: ["0%", "-100%"],
+      transition: {
+        duration: 15,
+        repeat: Infinity,
+        ease: "linear",
+      },
+    });
+  }, [controls]);
   return (
     <div className="">
-     
       <div className="py-4">
         <h1 className="text-3xl font-extrabold ">Offers on going</h1>
         <h1>Fresh Deals Blooming !</h1>
@@ -31,30 +43,49 @@ const OnGoingOffer = () => {
         <div className="overflow-x-auto scrollbar-hide rounded-2xl w-full">
           <motion.div
             className="flex space-x-4"
-            animate={{ x: ["0%", "-100%"] }}
-            transition={{
-              duration: 15,
-              repeat: Infinity,
-              ease: "linear",
-            }}
+            // animate={{ x: ["0%", "-100%"] }}
+            // transition={{
+            //   duration: 15,
+            //   repeat: Infinity,
+            //   ease: "linear",
+            // }}
+            animate={controls}
+            onMouseLeave={() =>
+              controls.start({
+                x: ["0%", "-100%"],
+                transition: {
+                  duration: 15,
+                  repeat: Infinity,
+                  ease: "linear",
+                },
+              })
+            }
+            onMouseEnter={() => controls.stop()}
           >
-            {offerPlants.map((plant) => (
+            {products.map((plant) => (
               <div
                 key={plant.id}
+                onClick={() => {
+                  setPlantId(plant.id);
+                  setView(true);
+                }}
                 className="bg-[#C1DCDC] md:min-w-1/2 min-w-5/6 rounded-2xl relative p-4  cursor-pointer"
               >
                 <h3 className="absolute md:right-1/4 md:top-1/2  bottom-1 right-0.5 text-right text-xl ">
                   <p>
-                    <span className="md:text-2xl font-semibold">{plant.plantName}</span>
+                    <span className="md:text-2xl font-semibold">
+                      {plant.plantName}
+                    </span>
                     <br />
-                    <span className="md:text-3xl font-bold">{plant.offer}% off</span>
+                    <span className="md:text-3xl font-bold">
+                      {plant.offer}% off
+                    </span>
                   </p>
                 </h3>
                 <img
-                src={plant.image?.img1}
+                  src={plant.image?.img1}
                   alt=""
                   className="rounded-2xl right-2 bottom-2 object-cover md:w-1/4 w-3/4 md:h-65 h-50"
-                  
                 />
               </div>
             ))}
@@ -64,6 +95,14 @@ const OnGoingOffer = () => {
         {/* Right arrow icon (optional, static) */}
         <FaAngleRight size={20} />
       </div>
+      {/* Product View Modal */}
+      {view && (
+        <ProductView
+          onExit={() => setView(false)}
+          products={products}
+          plantId={plantId}
+        />
+      )}
     </div>
   );
 };
